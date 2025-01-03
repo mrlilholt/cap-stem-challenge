4a2ebe265ac61ebbe6809145099b4590b8e9b16c
Lilholt <63080594+mrlilholt@users.noreply.github.com>
2025-01-02 20:38:26 -0500

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -8,15 +8,15 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase
 // Firebase Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAn89gsy3bcj6j1wGp_mEvdoGaYh9XyJAg",
-  authDomain: "cap-stem-challenge.firebaseapp.com",
-  projectId: "cap-stem-challenge",
-  storageBucket: "cap-stem-challenge.firebasestorage.app",
-  messagingSenderId: "286470006062",
-  appId: "1:286470006062:web:78e1472fc313d88f2206bf",
-  measurementId: "G-FZLR2BVWRD"
+    authDomain: "cap-stem-challenge.firebaseapp.com",
+    projectId: "cap-stem-challenge",
+    storageBucket: "cap-stem-challenge.firebasestorage.app",
+    messagingSenderId: "286470006062",
+    appId: "1:286470006062:web:78e1472fc313d88f2206bf",
+    measurementId: "G-FZLR2BVWRD"
 };
 
-// Initialize Firebase App
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
@@ -24,4 +24,5 @@ const storage = getStorage(app);
 const analytics = getAnalytics(app);
 const provider = new GoogleAuthProvider();
 
+// Export auth and provider
 export { auth, provider, db, storage };
