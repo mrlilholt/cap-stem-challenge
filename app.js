d3b5788b2a0b396fe4787e5625aed50d32046260
Lilholt <63080594+mrlilholt@users.noreply.github.com>
2025-01-02 20:33:30 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+//const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const analytics = getAnalytics(app);
